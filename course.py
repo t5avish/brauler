0eaa1013dab4859ba8ef56420c257618e577c42c
Avishay Halifa <avishay.halifa@gmail.com>
2023-09-15 18:01:39 +0300

    removed console testers because now tests from gui

--- course.py
+++ course.py
@@ -1,5 +1,4 @@
 from data_parser import Parser
-from bidi.algorithm import get_display
 
 class Lesson:
     def __init__(self, type, day, start, finish, lecturer):
@@ -8,9 +7,6 @@ class Lesson:
         self.start = start
         self.finish = finish
         self.lecturer = lecturer
-
-    def __str__(self):
-        return get_display(self.type + " " + self.day + " " + self.start + " " + self.finish + " " + self.lecturer  + "\n")
 
 class Course:
     def __init__(self, id):
@@ -33,7 +29,6 @@ class Course:
         self.practices.append(practice)
 
     def about_parser(self, about_list):
-        print(about_list[0])
         try:
             self.points = float(about_list[0].split(" ")[5])
             for index in range(1, len(about_list)):
@@ -63,17 +58,3 @@ class Course:
                     self.add_lab(lesson)
                 case _:
                     pass
-
-    def __str__(self):
-        str = ""
-        str += "Course: "  + "\n"
-        str += "LECS:" + "\n"
-        for lec in self.lectures:
-            str += lec.__str__() + "\n"
-        str += "PRACS:" + "\n"
-        for prac in self.practices:
-           str += prac.__str__()  + "\n"
-        str += "LABS:" + "\n"
-        for lab in self.labs:
-            str += lab.__str__()  + "\n"
-        return str
