63b77a9539620f0f720591a6503225aac39f5c24
Avishay Halifa <avishay.halifa@gmail.com>
2023-09-18 20:38:40 +0300

    added remove buttons features to course

--- gui.py
+++ gui.py
@@ -24,7 +24,6 @@ class GUI:
         self.top_left_frame = ctk.CTkFrame(master=self.left_frame)
         self.top_left_frame.pack(padx=10, pady=10, fill="both", expand="True")
 
-
         self.scheduler_button = ctk.CTkButton(master=self.left_frame , text="צור מערכת",font=("Narkisim",18), command=lambda : self.generate_schedule())
         self.scheduler_button.pack(pady=10, padx=10)
 
@@ -63,7 +62,7 @@ class GUI:
         course_frame = ctk.CTkFrame(master=self.top_right_frame)
         course_frame.pack(padx=10, pady=10,fill="both", expand="True")
 
-        course_remove_button = ctk.CTkButton(master=course_frame, text="הסר", font=("Narkisim", 18), command = lambda : course_frame.destroy())
+        course_remove_button = ctk.CTkButton(master=course_frame, text="הסר", font=("Narkisim", 18), command = lambda : self.delete_course(course_frame, course))
         course_remove_button.pack(pady=10, padx=10, side="left")
 
         about_button = ctk.CTkButton(master=course_frame, text="פרטים", font=("Narkisim", 18), command = lambda : self.course_info(course))
@@ -74,6 +73,10 @@ class GUI:
         course_label.pack(pady=10, padx=10, side="right")
 
         self.add_course_entry.delete(0, 'end')
+
+    def delete_course(self, frame, course):
+        self.courses.remove(course)
+        frame.destroy()
 
     def course_info(self, course):
         self.remove_all_widgets(self.top_left_frame)
@@ -124,47 +127,51 @@ class GUI:
 
     def generate_schedule(self):
         schedele, latest = scheduler(self.courses)
-        days = {'א' : 0, 'ב' : 1, 'ג' : 2, 'ד' : 3, 'ה' : 4, 'ו' : 5}
-        hours_dict = {"08:30": 0, "09:30": 1, "10:30" : 2 ,"11:30": 3,"12:20": 4, "12:50": 4, "13:50": 5, "14:50": 6, "15:50": 7, "16:50": 8, "17:50": 9, "18:50": 10, "19:50": 11, "20:50": 12, "21:50": 13}
-        hours_list = ["08:30", "09:30", "10:30", "11:30", "12:50", "13:50", "14:50", "15:50", "16:50", "17:50", "18:50", "19:50", "20:50", "21:50"]
 
-        for index, value in enumerate(hours_list):
-            if value == latest:
-                latest_index = index
+        if schedele:
+            days = {'א' : 0, 'ב' : 1, 'ג' : 2, 'ד' : 3, 'ה' : 4, 'ו' : 5}
+            hours_dict = {"08:30": 0, "09:30": 1, "10:30" : 2 ,"11:30": 3,"12:20": 4, "12:50": 4, "13:50": 5, "14:50": 6, "15:50": 7, "16:50": 8, "17:50": 9, "18:50": 10, "19:50": 11, "20:50": 12, "21:50": 13}
+            hours_list = ["08:30", "09:30", "10:30", "11:30", "12:50", "13:50", "14:50", "15:50", "16:50", "17:50", "18:50", "19:50", "20:50", "21:50"]
 
-        schedule_window = ctk.CTkToplevel()
-        hours_frame = ctk.CTkFrame(master=schedule_window)
-        hours_frame.pack(padx=10, pady=10, side="right", expand="True", fill="both")
+            for index, value in enumerate(hours_list):
+                if value == latest:
+                    latest_index = index
 
-        empty_label = ctk.CTkLabel(master=hours_frame, text="", font=('Narkisim', 30), justify="right")
-        empty_label.pack(pady=20, padx=10)
-        
-        labels_list = []
+            schedule_window = ctk.CTkToplevel()
+            hours_frame = ctk.CTkFrame(master=schedule_window)
+            hours_frame.pack(padx=10, pady=10, side="right", expand="True", fill="both")
 
-        for index in range(latest_index + 1):
-            hour_label = ctk.CTkLabel(master=hours_frame, text=hours_list[index], font=('Narkisim', 30), justify="right")
-            hour_label.pack(pady=20, padx=10)
-        for day in days:
-            day_frame = ctk.CTkFrame(master=schedule_window)
-            day_frame.pack(padx=10, pady=10, side="right", expand="True", fill="both")
+            empty_label = ctk.CTkLabel(master=hours_frame, text="", font=('Narkisim', 30), justify="right")
+            empty_label.pack(pady=20, padx=10)
+            
+            labels_list = []
 
-            days_label = ctk.CTkLabel(master=day_frame, text=day, font=('Narkisim', 30), justify="right")
-            days_label.pack(pady=20, padx=10)
-            day_labels = []
             for index in range(latest_index + 1):
-                label = ctk.CTkLabel(master=day_frame, text="-", font=('Narkisim', 16), justify="right")
-                day_labels.append(label)
-                label.pack(pady=20, padx=10)
-            labels_list.append(day_labels)
-        
-        for course, lessons in schedele:
-            course_name = course.name
-            for lesson in lessons:
-                type = lesson.type
-                lecturer = lesson.lecturer
-                day_index = days[lesson.day]
-                start_index = hours_dict[lesson.start]
-                finish_index = hours_dict[lesson.finish]
-                for i in range(start_index, finish_index):
-                    info = course_name + "\n" + type + "   " + lecturer
-                    labels_list[day_index][i].configure(text=info)
+                hour_label = ctk.CTkLabel(master=hours_frame, text=hours_list[index], font=('Narkisim', 30), justify="right")
+                hour_label.pack(pady=20, padx=10)
+            for day in days:
+                day_frame = ctk.CTkFrame(master=schedule_window)
+                day_frame.pack(padx=10, pady=10, side="right", expand="True", fill="both")
+
+                days_label = ctk.CTkLabel(master=day_frame, text=day, font=('Narkisim', 30), justify="right")
+                days_label.pack(pady=20, padx=10)
+                day_labels = []
+                for index in range(latest_index + 1):
+                    label = ctk.CTkLabel(master=day_frame, text="-", font=('Narkisim', 16), justify="right")
+                    day_labels.append(label)
+                    label.pack(pady=20, padx=10)
+                labels_list.append(day_labels)
+            
+            for course, lessons in schedele:
+                course_name = course.name
+                for lesson in lessons:
+                    type = lesson.type
+                    lecturer = lesson.lecturer
+                    day_index = days[lesson.day]
+                    start_index = hours_dict[lesson.start]
+                    finish_index = hours_dict[lesson.finish]
+                    for i in range(start_index, finish_index):
+                        info = course_name + "\n" + type + "   " + lecturer
+                        labels_list[day_index][i].configure(text=info)
+        else:
+            print("X")
